498e770f786c2465ee99a18dacaea777d8ef5a36
serjant007 <greatserjant007@gmail.com>
2021-02-20 23:23:53 +0700

    create application

--- src/app.jsx
+++ src/app.jsx
@@ -28,14 +28,14 @@ const App = () => {
 
   // </Get Data>
 
-  // <State>
+  // <Service-State>
 
   const [loading, setLoading] = useState(true);
   const [currentPage, setCurrentPage] = useState(1);
   const [cardsPerPage] = useState(66);
   const [cardView, setCardView] = useState('card');
   const [sort, setSort] = useState('category');
-  // </State>
+  // </Service-State>
 
   // <Pagination>
   const indexOfLastCard = currentPage * cardsPerPage;
